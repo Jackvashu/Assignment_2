f6a4e647e9b131418dbd37e0b09d492982371643
Vashu verma <38494928+Jackvashu@users.noreply.github.com>
2022-05-23 19:53:43 +0530

    Update A2q2.py

--- A2q2.py
+++ A2q2.py
@@ -2,6 +2,6 @@ dict = {}  # New Dictionary
 
 # range for 97 which is a ascii value 
 # from 113 z value
-for i in range(97, 113):  
+for i in range(97, 123):  
     dict[chr(i)] = i   # changing range value to a-z
 print(dict)  #printing dict 
